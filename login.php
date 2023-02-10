1c4c73d54f051f10f9583102ccf9da4d1ef87891
Rafael Fernandez <118098507+Rafa1921@users.noreply.github.com>
2023-02-10 10:13:55 +0800

    Add files via upload
    fr the val using PHP

--- login.php
+++ login.php
@@ -1,38 +1,67 @@
-<!DOCTYPE html>
-<?php 
- if(isset($_POST['submit'])){
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-    if($user=="rafael" && $pass=="fernandez"){
-        echo("Username and Password is correct");
-        }else{
-            echo("please enter again the correct data!");
-     } 
-    }
-     ?>
-<html lang="en">
+<?php session_start(); /* Starts the session */
+
+/* Check Login form submitted */
+if (isset($_POST['Submit'])) {
+  /* Define username and associated password array */
+  $logins = array('Alex' => '123456', 'username1' => 'password1', 'username2' => 'password2');
+
+  /* Check and assign submitted Username and Password to new variable */
+  $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+  $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+  /* Check Username and Password existence in defined array */
+  if (isset($logins[$Username]) && $logins[$Username] == $Password) {
+    /* Success: Set session variables and redirect to Protected page  */
+    $_SESSION['UserData']['Username'] = $logins[$Username];
+    header("location:index.php");
+    exit;
+  } else {
+    /*Unsuccessful attempt: Set error message */
+    $msg = "<span style='color:red'>Invalid Login Details</span>";
+  }
+}
+?>
+<!doctype html>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <title>PHP Login Script Without Using Database</title>
+  <link href="./css/style.css" rel="stylesheet">
 </head>
+
 <body>
-    <form action="" method="post">
-    <table align="center">
+  <div id="Frame0">
+    <h1>PHP Login Script Without Using Database Demo.</h1>
+
+  </div>
+  <br>
+  <form action="" method="post" name="Login_Form">
+    <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+      <?php if (isset($msg)) { ?>
         <tr>
-            <td>Username:</td>
-            <td><input type="text" name="username" placeholder="Enter your username">
+          <td colspan="2" align="center" valign="top"><?php echo $msg; ?></td>
         </tr>
-        <tr>
-            <td>Paswword:</td>
-            <td><input type="password" placeholder="Enter your passowrd"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="submit"></td>
-        </tr>
-            
+      <?php } ?>
+      <tr>
+        <td colspan="2" align="left" valign="top">
+          <h3>Login</h3>
+        </td>
+      </tr>
+      <tr>
+        <td align="right" valign="top">Username</td>
+        <td><input name="Username" type="text" class="Input"></td>
+      </tr>
+      <tr>
+        <td align="right">Password</td>
+        <td><input name="Password" type="password" class="Input"></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+      </tr>
     </table>
+  </form>
 </body>
+
 </html>
